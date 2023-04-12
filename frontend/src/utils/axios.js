626f7a14576ddfee1d99acd2dcda7a3ca6d27974
Jainil Sevalia <jn498899@dal.ca>
2023-04-12 11:36:19 -0300

    Backend Url Updated

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,6 +1,7 @@
 import Axios from "axios";
 
 export const axios = Axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL,
+  // baseURL: process.env.REACT_APP_BACKEND_URL,
+  baseURL: "http://localhost:5000",
   withCredentials: true,
 });
