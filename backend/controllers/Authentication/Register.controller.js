9634f02233c1f723b9d737c93b2be92ea7671f71
Jainil Sevalia <jn498899@dal.ca>
2023-04-12 20:41:43 -0300

    mail changes

--- backend/controllers/Authentication/Register.controller.js
+++ backend/controllers/Authentication/Register.controller.js
@@ -87,32 +87,38 @@ exports.registerUser = async (req, res) => {
           //   maxAge: tokenAge * 24 * 60 * 60 * 1000,
           //   sameSite: "lax",
           // });
-          axios.post(
-            process.env.API_GATEWAY_URL ||
-              "https://40e3pgta0h.execute-api.us-east-1.amazonaws.com/dev" +
-                "/SendMailShiftSync",
-            {
-              to: newImg.email,
-              subject: `${newImg.name} Successfully Registered on Shift Sync as ${newImg.Position} in ${newImg.role}`,
-              message: `Dear ${newImg.name},
-
-            We are delighted to inform you that your registration on our Shift-Sync  has been successful. As a Manager or Employee, you now have access to all the features and functionality of our web application, which has been designed to make shift management easier and more efficient.
-
-            With our web application, you can manage your shifts, view your schedule, request time off, and communicate with your team members seamlessly. As a Manager, you also have additional capabilities such as shift assignment, managing employee requests, and approving time off requests.
-
-            We understand that effective shift management is critical to the success of any business, and we are committed to providing you with the tools you need to streamline this process. Our user-friendly interface and customer support team are available to assist you in case you encounter any issues or have any questions.
-
-            To get started, please use the login credentials provided during the registration process to access your account. Once logged in, you will have access to all the features and functionality available to your role.
-
-            We appreciate your decision to choose our web application for your shift management needs, and we are confident that it will help simplify your work. If you have any feedback or suggestions on how we can improve our services, please do not hesitate to let us know.
-
-            Thank you for joining our community. We look forward to serving you.
-
-            Best regards,
-
-            Shift-Sync`,
-            }
-          );
+          try {
+            axios.post(
+              process.env.API_GATEWAY_URL ||
+                "https://40e3pgta0h.execute-api.us-east-1.amazonaws.com/dev" +
+                  "/SendMailShiftSync",
+              {
+                to: newImg.email,
+                subject: `${newImg.name} Successfully Registered on Shift Sync as ${newImg.Position} in ${newImg.role}`,
+                message: `Dear ${newImg.name},
+  
+              We are delighted to inform you that your registration on our Shift-Sync  has been successful. As a Manager or Employee, you now have access to all the features and functionality of our web application, which has been designed to make shift management easier and more efficient.
+  
+              With our web application, you can manage your shifts, view your schedule, request time off, and communicate with your team members seamlessly. As a Manager, you also have additional capabilities such as shift assignment, managing employee requests, and approving time off requests.
+  
+              We understand that effective shift management is critical to the success of any business, and we are committed to providing you with the tools you need to streamline this process. Our user-friendly interface and customer support team are available to assist you in case you encounter any issues or have any questions.
+  
+              To get started, please use the login credentials provided during the registration process to access your account. Once logged in, you will have access to all the features and functionality available to your role.
+  
+              We appreciate your decision to choose our web application for your shift management needs, and we are confident that it will help simplify your work. If you have any feedback or suggestions on how we can improve our services, please do not hesitate to let us know.
+  
+              Thank you for joining our community. We look forward to serving you.
+  
+              Best regards,
+  
+              Shift-Sync`,
+              }
+            );
+          } catch (err) {
+            res
+              .status(201)
+              .json({ newImg, message: `${name} registerd Successfully` });
+          }
           res
             .status(201)
             .json({ newImg, message: `${name} registerd Successfully` });
